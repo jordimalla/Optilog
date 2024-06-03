edcf49acca09b5b12f6a07043a45a64e1db1dad6
Jordi Malla <jsanto82@gmail.com>
2024-06-03 22:19:31 +0200

    Created initial model

--- Optilog/settings.py
+++ Optilog/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'opti.apps.OptiConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- opti/models.py
+++ opti/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+class WeeklyTransportCapacity(models.Model):
+    week_of_year = models.IntegerField()
+    max_transport_capacity = models.FloatField(default=0)
+
+class Slaugherhouse(models.Model):
+    name = models.CharField(max_length=300)
+
+class Farm(models.Model):
+    name = models.CharField(max_length=300)
+
+class WeeklyAnimalTransport(models.Model):
+    origin_farm = models.ForeignKey(Farm, on_delete=models.DO_NOTHING)
+    destination_slaughterhouse = models.ForeignKey(Slaugherhouse, on_delete=models.DO_NOTHING)
+    transport_cost = models.FloatField(default=0)
+    week_of_year = models.IntegerField()
+
+class WeeklySlaughterhouseDemand(models.Model):
+    slaughterhouse = models.ForeignKey(Slaugherhouse, on_delete=models.DO_NOTHING)
+    animal_count = models.IntegerField(default=0)
+    live_animal_price_per_kg = models.FloatField(default=0)
+    week_of_year = models.IntegerField()
+
+class WeeklyFarmAnimalAvailability(models.Model):
+    farm = models.ForeignKey(Farm, on_delete=models.DO_NOTHING)
+    animal_count = models.IntegerField(default=0)
+    average_weight = models.FloatField(default=0)
+    week_of_year = models.IntegerField()
