ffc100bf02a1def1a9d6e116d9f9ba3b3f9cfe23
Jordi Malla <jsanto82@gmail.com>
2024-06-03 23:14:26 +0200

    Add admin views of opti models

--- opti/admin.py
+++ opti/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import WeeklyTransportCapacity, Slaugherhouse, Farm, WeeklyAnimalTransport, WeeklySlaughterhouseDemand, WeeklyFarmAnimalAvailability
 
 # Register your models here.
+admin.site.register(WeeklyAnimalTransport)
+admin.site.register(Slaugherhouse)
+admin.site.register(Farm)
+admin.site.register(WeeklyFarmAnimalAvailability)
+admin.site.register(WeeklySlaughterhouseDemand)
+admin.site.register(WeeklyTransportCapacity)
--- opti/models.py
+++ opti/models.py
@@ -4,27 +4,39 @@ from django.db import models
 class WeeklyTransportCapacity(models.Model):
     week_of_year = models.IntegerField()
     max_transport_capacity = models.FloatField(default=0)
+    def __str__(self):
+        return str(self.week_of_year)
 
 class Slaugherhouse(models.Model):
     name = models.CharField(max_length=300)
+    def __str__(self):
+        return self.name
 
 class Farm(models.Model):
     name = models.CharField(max_length=300)
+    def __str__(self):
+        return self.name
 
 class WeeklyAnimalTransport(models.Model):
     origin_farm = models.ForeignKey(Farm, on_delete=models.DO_NOTHING)
     destination_slaughterhouse = models.ForeignKey(Slaugherhouse, on_delete=models.DO_NOTHING)
     transport_cost = models.FloatField(default=0)
     week_of_year = models.IntegerField()
+    def __str__(self):
+        return str(self.week_of_year) + '_' + self.origin_farm.name + '_' + self.destination_slaughterhouse.name
 
 class WeeklySlaughterhouseDemand(models.Model):
     slaughterhouse = models.ForeignKey(Slaugherhouse, on_delete=models.DO_NOTHING)
     animal_count = models.IntegerField(default=0)
     live_animal_price_per_kg = models.FloatField(default=0)
     week_of_year = models.IntegerField()
+    def __str__(self):
+        return str(self.week_of_year) + '_' + self.slaughterhouse.name
 
 class WeeklyFarmAnimalAvailability(models.Model):
     farm = models.ForeignKey(Farm, on_delete=models.DO_NOTHING)
     animal_count = models.IntegerField(default=0)
     average_weight = models.FloatField(default=0)
     week_of_year = models.IntegerField()
+    def __str__(self):
+        return str(self.week_of_year) + '_' + self.farm.name
