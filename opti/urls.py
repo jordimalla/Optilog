55e2ffd9f50adee0eb547e448b5e898a28d36dfb
Jordi Malla <jsanto82@gmail.com>
2024-06-05 23:41:23 +0200

    Form optimize created

--- opti/urls.py
+++ opti/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = "opti"
 urlpatterns = [
     path("", views.index, name="index"),
+    path("detail/", views.detail, name="detail"),
+    path("optimize/", views.optimize, name="optimize"),
 ]
--- opti/views.py
+++ opti/views.py
@@ -1,4 +1,24 @@
 from django.http import HttpResponse
+from django.http import Http404
+from django.shortcuts import render
+
+from .models import WeeklyTransportCapacity, Slaugherhouse, Farm, WeeklyAnimalTransport, WeeklySlaughterhouseDemand, WeeklyFarmAnimalAvailability
 
 def index(request):
     return HttpResponse("Hellow, world. You're at the opti index.")
+
+def detail(request):
+    try:
+        list_week_of_year_available = WeeklyTransportCapacity.objects.order_by('week_of_year').values_list('week_of_year', flat=True).distinct('week_of_year')
+    except WeeklyTransportCapacity.DoesNotExist:
+        raise Http404("Weekly transport capacity doesn't exist")
+    return render(request, "opti/detail.html", {"weeksOfYear": list_week_of_year_available})
+
+def optimize(request):
+    try:
+        list_week_of_year_available = WeeklyTransportCapacity.objects.order_by('week_of_year').values_list('week_of_year', flat=True).distinct('week_of_year')
+        week_of_year_selected = request.POST["weekOfYear"]
+        list_week_animal_transport = WeeklyAnimalTransport.objects.filter(week_of_year=week_of_year_selected)
+    except WeeklyTransportCapacity.DoesNotExist:
+        raise Http404("Weekly transport capacity doesn't exist")
+    return render(request, "opti/detail.html", {"weeksOfYear": list_week_of_year_available, "weekAnimalTransports": list_week_animal_transport})
